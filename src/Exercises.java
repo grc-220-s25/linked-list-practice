c69d8427e6d94738bebd7b6de17b3634874459fa
Masih <masihrahmani87@gmail.com>
2025-05-13 20:38:11 -0700

    Add sum methods to calculate the total value of linkedlist

--- src/Exercises.java
+++ src/Exercises.java
@@ -12,7 +12,14 @@ public class Exercises {
      * @return sum of the values in the list
      */
     public static int sum(ListNode head) {
-        return -1;
+        int sum = 0;
+    ListNode curr = head;
+    while (curr != null) {
+        sum += curr.data;
+        curr = curr.next;
+    }
+    return sum;
+
     }
 
     /**
